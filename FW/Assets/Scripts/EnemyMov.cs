3e3a3c95ca41394dd7091dfc4eed397b4bc3664e
LuCheMaru <lucca07@outlook.com>
2024-10-17 18:31:59 -0300

    AAAAAAAA

---
+++ FW/Assets/Scripts/EnemyMov.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inimigo : MonoBehaviour
+{
+    [SerializeField]
+    public float spd = 2f;
+
+    private Transform target;
+    private int wavepointIndex = 0;
+
+    void Start()
+    {
+        target = Waypoint.waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * spd * Time.deltaTime, Space.World);
+
+        /*if (Vector3.Distance(transform.position, target.position) <= 0.4f)
+        {
+            Destroy(gameObject);
+        };*/
+    }
+}
